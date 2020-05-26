5108cc35a7b2e8013afe72e3d25ba09bc8ad289c
Pranati219 <65551185+Pranati219@users.noreply.github.com>
2020-05-27 01:02:33 +0530

    Portola First Draft in Progress
    Started automation using selenium

---
+++ Portola.java
@@ -0,0 +1,30 @@
+package test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Portola {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//String a="P0rt0la123";
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://173.255.219.239:8080/return/");
+		//driver.findElement(By.xpath("//*[@id='password_protected_pass']")).click();
+		//driver.findElement(By.xpath("//*[@id='password_protected_pass']")).sendKeys("P0rt0la123");
+		driver.findElement(By.id("password_protected_pass")).sendKeys("P0rt0la123");
+		
+		driver.findElement(By.xpath("//*[@id='wp-submit']")).click();
+		driver.findElement(By.xpath("//*[@id='request_type']/div[3]/div[1]/label']")).click();
+		//driver.findElement(By.className("onclick")).click();
+		//driver.findElement(By.xpath("//*[@id='request_type']/div[4]/a")).click();
+		//Multiple window concept-
+		//driver.findElement(By.xpath("//*[@id='cookie_action_close_header']")).click();
+		//driver.findElement(By.cssSelector("#request_type > div.pr-btn-row > a")).click();
+		//driver.findElement(By.xpath("//*[@id='cookie_action_close_header']")).click();
+
+	}
+
+}
